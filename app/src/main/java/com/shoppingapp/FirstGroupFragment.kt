f3dbc636dda6b047a4bb91dcad8b69ebe099079a
huzzy <hasanyildiz5151@gmail.com>
2023-03-28 23:08:26 +0600

    point

--- app/src/main/java/com/shoppingapp/FirstGroupFragment.kt
+++ app/src/main/java/com/shoppingapp/FirstGroupFragment.kt
@@ -1,10 +1,13 @@
 package com.shoppingapp
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.collection.arraySetOf
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.database.*
@@ -12,6 +15,7 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import com.shoppingapp.databinding.FragmentFirstGroupBinding
+import kotlinx.coroutines.delay
 
 
 class FirstGroupFragment : Fragment() {
@@ -20,7 +24,7 @@ class FirstGroupFragment : Fragment() {
     private lateinit var database: FirebaseDatabase
     private val universityList = arrayListOf<UniversityModel>()
     private var point : Double = 0.0
-    private val uniIdList  = hashSetOf<Int>()
+    private val uniIdList  = arraySetOf<Int>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,16 +49,13 @@ class FirstGroupFragment : Fragment() {
              database.getReference("faculty").orderByChild("nonDsPoint").startAt(point)
                  .addChildEventListener(object : ChildEventListener {
                      override fun onChildAdded(dataSnapshot: DataSnapshot, prevChildKey: String?) {
-                         Log.e("point","onchildadded")
                          val facultyModel  = dataSnapshot.getValue<FacultyModel>()
                          if (facultyModel != null) {
                              if(!uniIdList.contains(facultyModel.uniId)){
                                  uniIdList.add(facultyModel.uniId!!)
-                                 getUniversity()
                              }
                          }
-
-                     } // ...
+                     }
 
                      override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
                      }
@@ -68,6 +69,11 @@ class FirstGroupFragment : Fragment() {
                      override fun onCancelled(error: DatabaseError) {
                      }
                  })
+
+             Handler(Looper.getMainLooper()).postDelayed({
+                 getUniversity()
+             },1000)
+
 
          }
          catch (e:java.lang.NumberFormatException){
@@ -92,15 +98,23 @@ fun setRw() {
 
 }
     fun getUniversity(){
+/*
         uniIdList.distinct().forEach {
+            Log.d("uniList","$it")
+        }
+*/
 
+        uniIdList.forEach {
             database.getReference("university").child(it.toString()).addListenerForSingleValueEvent(object:ValueEventListener{
                 override fun onDataChange(snapshot: DataSnapshot) {
                     val universityModel = snapshot.getValue<UniversityModel>()
                     if (universityModel != null) {
                         universityList.add(universityModel)
                     }
+
                     universityAdapter.updateList(universityList)
+
+
                 }
 
                 override fun onCancelled(error: DatabaseError) {
